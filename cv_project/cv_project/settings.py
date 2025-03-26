7601401ed274a1a13cfe6470ca17aaad8f37b88a
idahoh otis <idahohotis123@gmail.com>
2025-03-26 11:34:51 +0100

    Added Gunicorn

--- cv_project/cv_project/settings.py
+++ cv_project/cv_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-%uuv)ihd*)f60gh))pj70l0xja63r8!j!g#4qgwsh)m)eyjh+j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+PORT = os.getenv('PORT', '8000')
