fa552771560de4adfe2c138e1171b096b46299cf
idahoh otis <idahohotis123@gmail.com>
2025-03-27 17:07:59 +0100

    modification

--- cv_project/cv_project/settings.py
+++ cv_project/cv_project/settings.py
@@ -12,21 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
 
 
 
